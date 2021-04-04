89f8489eb5fd3f961c4928b1f39cc00d3df1705f
GaelGabriel <81935506+GaelGabriel@users.noreply.github.com>
2021-04-04 17:38:54 -0600

    Add files via upload

---
+++ oswaldoortizb2/src/oswaldoortizb2/Oswaldoortizb2.java
@@ -0,0 +1,10 @@
+
+package oswaldoortizb2;
+public class Oswaldoortizb2 {
+    public static void main(String[] args) {
+        
+        System.out.println("Oswaldo Gael Ortiz Gabriel");
+      
+    }
+    
+}
